f351cee91bcff019fd52c82189e1fd984d7a2130
Yongfu <rickie622@gmail.com>
2017-06-07 14:40:48 +0800

    first commit

---
+++ lnmp-init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## 定义环境变量
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
+export LANG="en_US.UTF-8"
+
+## 该脚本需要以root用户运行
+user=$(whoami)
+if [ $user != 'root' ] ; then
+    echo "该脚本需要以root用户运行, 当前用户为 $user"
+    exit 1;
+fi
+
+## 更新系统到最新
+echo "LOC-lnmp: 正在更新系统... "
+yum -y update
+
+## 安装依赖
+echo "LOC-lnmp: 正在安装依赖包... "
+yum install -y wget epel-release
+yum clean all
+
+
+## 关闭selinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+echo "[7]关闭selinux"
+
+## 修改 ulimit 配置
+echo "* soft nofile 65535" >> /etc/security/limits.conf
+echo "* hard nofile 65536" >> /etc/security/limits.conf
+echo "[8]调整文件描述符数量"
+
+sleep 2
---
+++ readme.md
@@ -0,0 +1,22 @@
+
+### 环境介绍
+- 系统环境: CentOS 7 64bit
+
+
+### 安装说明
+#### 系统初始化
+此阶段将只关注和lnmp相关的系统初始化。
+
+
+### 配置
+- lnmp安装配置
+- 各软件对应的配置文件
+
+### 安装
+默认安装：
+curl -sk https://raw.githubusercontent.com/RickieL/centos-sa/master/lnmp-init.sh | sh -s " -N -M -P "
+
+
+### todo-list
+- loc-lnmp命令
+- loc-lnmp命令将支持单个软件的安装/卸载/升级
